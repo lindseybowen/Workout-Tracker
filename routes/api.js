d1a386b83a9acc1ed9dbaf5e37192afc5b338f7b
lindsey Bowen <lindseyjbowen@gmail.com>
2021-03-08 19:45:48 -0500

    added modules, server and started routs

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const router = require ("express").Router()
+
+const db = require("../models")
+
+router.get ("/api/workouts", function (req, res){
+    db.Workout.find().then(function(result){
+        res.json(result)
+    })
+})
+
+
+
+
+
+
+module.exports = router 
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require ("express")
+const app = express()
+
+const PORT = process.env.PORT || 3001 
+
+app.use(express.static("public"))
+
+//data paser - unwrap client data to stores in req.body
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout")
+
+app.use(require("./routes/api"))
+app.use(require("./routes/html"))
+
+app.listen(PORT,function(){
+    console.log("app is listening on"+PORT)
+}) 
+
