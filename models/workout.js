8b824dcd0dbba6ec13b416fbde75afba60f70c4e
LindseyBowen <lindseyjbowen@gmail.com>
2021-03-11 18:54:35 -0500

    had an issue with getting a new workout fixed api routs

--- models/workout.js
+++ models/workout.js
@@ -12,3 +12,4 @@ const workoutSchema = new Schema({
 
 const Workout = mongoose.model("Workout",workoutSchema)
 module.exports= Workout 
+
--- routes/html.js
+++ routes/html.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
- 
 const path = require("path")
+
 router.get ("/",function(req,res){
    res.sendFile(path.join(__dirname,"../public/index.html") ) 
 })
